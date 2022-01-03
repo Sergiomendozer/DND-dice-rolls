1b45093a6b34d38f2c289cdb823c9426eab56da2
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-03 12:56:53 -0800

    nearly done

--- DND_dice.py
+++ DND_dice.py
@@ -1,19 +1,23 @@
 import random
-# how many rolls
-# what range 
 def dnd_dice(range_of_dice,number_of_rolls,rolls_list,i):
     if i != number_of_rolls:
         i += 1
         roll = random.randint(0,range_of_dice)
         rolls_list.append(roll)
         
-        print (number_of_rolls)
-        print (rolls_list)
-        print (i)
+        # print (number_of_rolls)
+        # print (rolls_list)
+        # print (i)
         return (dnd_dice(range_of_dice,number_of_rolls,rolls_list,i))
     else:
-        print (rolls_list)
-        print("count for loop")
+        sum = 0
+        for e in rolls_list:
+            e = int(e)
+            sum = sum + e
+        sum = str(sum)
+        rolls_list = str(rolls_list)
+        print ("Your Rolls: " + rolls_list)
+        print ("Sum of rolls: " + sum)
 
 range_of_dice = int(input("What range is your dice:"))
 number_of_rolls = int(input("How many rolls:"))
