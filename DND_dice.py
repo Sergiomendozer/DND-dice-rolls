d7c11dd3f3af99aa89e8481ce4e0b474d32f5b05
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-03 12:46:58 -0800

    keeps track of how many rolls

--- DND_dice.py
+++ DND_dice.py
@@ -1,15 +1,18 @@
 import random
 # how many rolls
 # what range 
-def dnd_dice(range_of_dice,number_of_rolls,rolls_list):
+def dnd_dice(range_of_dice,number_of_rolls,rolls_list,i):
+    i += 1
     roll = random.randint(0,range_of_dice)
     rolls_list.append(roll)
-    print (roll)
+    
     print (number_of_rolls)
     print (rolls_list)
+    print (i)
     
 range_of_dice = int(input("What range is your dice:"))
 number_of_rolls = int(input("How many rolls:"))
 rolls_list = []
-dnd_dice(range_of_dice,number_of_rolls,rolls_list)
+i = 0
+dnd_dice(range_of_dice,number_of_rolls,rolls_list,i)
     
