b06431245988307463adc4b69712c874982dd14f
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-03 12:35:01 -0800

    function made

--- DND_dice.py
+++ DND_dice.py
@@ -1,2 +1,9 @@
-#new
-nklnkl
+import random
+# how many rolls
+# what range 
+def dnd_dice(range_of_dice):
+    print (range_of_dice)
+    
+range_of_dice = int(input("What range is your dice:"))
+dnd_dice(range_of_dice)
+    
