62316f7cf9ab37feed7a3ea8c18d734f9358ec4c
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-03 12:37:51 -0800

    takes a range and will give random number

--- DND_dice.py
+++ DND_dice.py
@@ -2,7 +2,8 @@ import random
 # how many rolls
 # what range 
 def dnd_dice(range_of_dice):
-    print (range_of_dice)
+    roll = random.randint(0,range_of_dice)
+    print (roll)
     
 range_of_dice = int(input("What range is your dice:"))
 dnd_dice(range_of_dice)
