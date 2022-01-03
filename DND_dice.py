c2fea779c52c1a3da721f8a71e6178192e9d7f47
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-03 12:41:35 -0800

    input of how many rolls is taken

--- DND_dice.py
+++ DND_dice.py
@@ -1,10 +1,13 @@
 import random
 # how many rolls
 # what range 
-def dnd_dice(range_of_dice):
+def dnd_dice(range_of_dice,number_of_rolls):
     roll = random.randint(0,range_of_dice)
     print (roll)
+    print (number_of_rolls)
     
 range_of_dice = int(input("What range is your dice:"))
-dnd_dice(range_of_dice)
+number_of_rolls = int(input("How many rolls:"))
+rolls_list = []
+dnd_dice(range_of_dice,number_of_rolls)
     
