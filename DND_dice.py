fdb3705e332710bdc0d7d9de39d500d0f7c796c2
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-03 12:51:49 -0800

    while loop implement for keeping track of how many rolls

--- DND_dice.py
+++ DND_dice.py
@@ -2,14 +2,19 @@ import random
 # how many rolls
 # what range 
 def dnd_dice(range_of_dice,number_of_rolls,rolls_list,i):
-    i += 1
-    roll = random.randint(0,range_of_dice)
-    rolls_list.append(roll)
-    
-    print (number_of_rolls)
-    print (rolls_list)
-    print (i)
-    
+    if i != number_of_rolls:
+        i += 1
+        roll = random.randint(0,range_of_dice)
+        rolls_list.append(roll)
+        
+        print (number_of_rolls)
+        print (rolls_list)
+        print (i)
+        return (dnd_dice(range_of_dice,number_of_rolls,rolls_list,i))
+    else:
+        print (rolls_list)
+        print("count for loop")
+
 range_of_dice = int(input("What range is your dice:"))
 number_of_rolls = int(input("How many rolls:"))
 rolls_list = []
