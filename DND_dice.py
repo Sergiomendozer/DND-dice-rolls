cc5f5c6729e278d72121d7a423150bd73aaa0cf5
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-03 12:44:50 -0800

    rolls append to list

--- DND_dice.py
+++ DND_dice.py
@@ -1,13 +1,15 @@
 import random
 # how many rolls
 # what range 
-def dnd_dice(range_of_dice,number_of_rolls):
+def dnd_dice(range_of_dice,number_of_rolls,rolls_list):
     roll = random.randint(0,range_of_dice)
+    rolls_list.append(roll)
     print (roll)
     print (number_of_rolls)
+    print (rolls_list)
     
 range_of_dice = int(input("What range is your dice:"))
 number_of_rolls = int(input("How many rolls:"))
 rolls_list = []
-dnd_dice(range_of_dice,number_of_rolls)
+dnd_dice(range_of_dice,number_of_rolls,rolls_list)
     
