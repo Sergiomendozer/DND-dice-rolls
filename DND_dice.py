b1dec7e9c792d859bd658e3bab3d8c3a9552dfd0
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-05 00:24:30 -0800

    reformatted

--- DND_dice.py
+++ DND_dice.py
@@ -1,26 +1,28 @@
 import random
-def dnd_dice(range_of_dice,number_of_rolls,rolls_list,i):
+
+
+def dnd_dice(range_of_dice, number_of_rolls, rolls_list, i):
     if i != number_of_rolls:
         i += 1
         # randomly selects a number between 0 and wht user chose
-        roll = random.randint(0,range_of_dice)
+        roll = random.randint(0, range_of_dice)
         rolls_list.append(roll)
-        return (dnd_dice(range_of_dice,number_of_rolls,rolls_list,i))
+        return dnd_dice(range_of_dice, number_of_rolls, rolls_list, i)
     else:
-        # for loop sums up all the rolls 
+        # for loop sums up all the rolls
         sum = 0
         for e in rolls_list:
             e = int(e)
             sum = sum + e
         sum = str(sum)
         rolls_list = str(rolls_list)
-        print ("Your Rolls: " + rolls_list)
-        print ("Sum of rolls: " + sum)
+        print("Your Rolls: " + rolls_list)
+        print("Sum of rolls: " + sum)
+
 
 # prompts user to input what is asked
 range_of_dice = int(input("What range is your dice:"))
 number_of_rolls = int(input("How many rolls:"))
 rolls_list = []
 i = 0
-dnd_dice(range_of_dice,number_of_rolls,rolls_list,i)
-    
+dnd_dice(range_of_dice, number_of_rolls, rolls_list, i)
